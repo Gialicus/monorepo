90693916d9a628bee2ab59b256904936e66a6884
g.coletta <giali.coletta@fullstackagency.it>
2023-12-29 18:54:19 +0100

    add login

--- apps/api/src/app/routes/auth/login/index.ts
+++ apps/api/src/app/routes/auth/login/index.ts
@@ -1,6 +1,15 @@
 import { FastifyPluginAsync } from 'fastify';
 import { JsonSchemaToTsProvider } from '@fastify/type-provider-json-schema-to-ts';
 import { LoginSchema } from '@monorepo/schemas';
+import { MONGO_COLLECTIONS } from '@monorepo/interfaces';
+import { createHash } from 'node:crypto';
+
+function hashPassword(password) {
+  const hash = createHash('sha256');
+  hash.update(password + process.env.SALT_KEY);
+  const hashedPassword = hash.digest('hex');
+  return hashedPassword;
+}
 
 const login: FastifyPluginAsync = async (fastify): Promise<void> => {
   const app = fastify.withTypeProvider<JsonSchemaToTsProvider>();
@@ -13,7 +22,13 @@ const login: FastifyPluginAsync = async (fastify): Promise<void> => {
     },
     async function (request) {
       const { email, password } = request.body;
-      return { token: email + password };
+      const user = await fastify.mongo.db
+        .collection(MONGO_COLLECTIONS.USERS)
+        .findOne({ email });
+      if (!user || user.password !== hashPassword(password)) {
+        throw fastify.httpErrors.unauthorized;
+      }
+      return { token: fastify.jwt.sign({ email }) };
     }
   );
 };
